7b7e09857b9cfab5efd7d9d85235582b51b81444
Chung Wu <chungwu@plasmic.app>
2022-05-20 17:33:18 -0700

    Make model-schema also work with plural model names

--- server/services/model-schema-service.js
+++ server/services/model-schema-service.js
@@ -2,8 +2,28 @@
 
 module.exports = ({ strapi }) => ({
   getModelSchema(model) {
-    const schema = strapi.api[model].contentTypes[model];
+    const schema = findModelSchema(strapi, model);
+    if (!schema) {
+      return undefined;
+    }
     const {__schema__, ...rest} = schema;
     return rest;
   },
 });
+
+function findModelSchema(strapi, model) {
+  // fast path: model name is singular
+  if (strapi.api[model] && strapi.api[model].contentTypes[model]) {
+    return strapi.api[model].contentTypes[model];
+  }
+
+  // slow path: loop through all models, look by both singular and plural
+  for (const apiSurface of Object.values(strapi.api)) {
+    for (const contentType of Object.values(apiSurface.contentTypes)) {
+      if (contentType.info.singularName === model || contentType.info.pluralName === model) {
+        return contentType;
+      }
+    }
+  }
+  return undefined;
+}
